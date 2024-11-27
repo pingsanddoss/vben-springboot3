ef8a27591048da5c8bf3863695672583c944cef5
pings <1262604140@qq.com>
2024-11-27 14:18:59 +0800

    Initial commit

---
+++ src/main/java/org/vben/vbenspringboot3/VbenSpringboot3Application.java
@@ -0,0 +1,13 @@
+package org.vben.vbenspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbenSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VbenSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/vben/vbenspringboot3/VbenSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vben.vbenspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbenSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
